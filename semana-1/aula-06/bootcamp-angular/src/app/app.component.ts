369abcfdc83b2c1b7ace35b861f0db88c3237324
caueonofri <caue.onofri@gmail.com>
2023-01-14 13:15:37 -0300

    angular project with material setup

---
+++ semana-1/aula-06/bootcamp-angular/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  longText= `shiba inu asldf asdf asg dg asdg adgasdg asdg asdg asdgasjlkg askg askljg askdjg asgklasjdg askldjg as`;
+  isHidden = true;
+}
---
+++ semana-1/aula-06/bootcamp-angular/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppHeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    NoopAnimationsModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
